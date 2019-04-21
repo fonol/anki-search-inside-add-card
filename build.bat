6c0fed817edcf17b6d1e55bc5625b5371d02124f
Tom Zeller <tom-kouran@gmx.de>
2019-04-21 11:28:39 +0200

    add build script

---
+++ build.bat
@@ -0,0 +1,2 @@
+set path="C:\Program Files\WinRAR\";%path%
+WinRAR a -afzip -ep1 -ibck -r -y "addon-packed.zip" -x.gitignore -x*.zip -x__pycache__ -xindex -x*.bat -x*.txt -x*.db -xmeta.json -x\.git -x\.vscode
