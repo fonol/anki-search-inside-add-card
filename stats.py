39f3bebf0cce4c1849c288bcf28b939a19f4f5f9
Tom Zeller <tom-kouran@gmx.de>
2019-04-30 12:04:50 +0200

    rework stats to be printed in modal

--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
 from aqt import mw
+import time
 
 
 def findNotesWithLowestPerformance(decks, limit, retOnly = False):
@@ -101,112 +102,181 @@ def _getScore(cards, onlyRet = False):
     return _calcPerformanceScore(retention, avgTime, goodAndEasy, hard)
 
 
-def calculateStats(nid, avgRetAndTime = None):
+def calculateStats(nid, gridView):
+    
+    tables = []
+    infoTable = {}
+    infoTable["Note ID"] = nid
+
+    note = mw.col.getNote(nid)
+    model = mw.col.models.get(note.mid)
+    templates = mw.col.findTemplates(note)
+
+    try:
+        infoTable["Created Date"] = time.strftime("%Y-%m-%d", time.localtime(int(nid)/1000)) + " &nbsp;&nbsp;<a href='#' style='' onclick='pycmd(\"addedSameDay %s\"); $(\"#a-modal\").hide(); return false;'>Added Same Day</a>" % nid
+        infoTable["Last Modified"] = time.strftime("%Y-%m-%d", time.localtime(note.mod))
+    except:
+        pass
+    if model is not None:
+        infoTable["Note Type"] = model["name"]
+
+
     #get card ids for note
     cards = mw.col.db.all("select * from cards where nid = %s" %(nid))
     if not cards:
-        return ""
+        infoTable["Result"] = "No cards found"
+        tables.append(infoTable)
+        return _buildTable(tables)
+    cardOrdById = {}
+    cardTypeById = {}
     cStr = "("
     for c in cards:
         cStr += str(c[0]) + ", "
+        cardOrdById[c[0]] = c[3]
+        cardTypeById[c[0]] = _cardTypeStr(c[6])
     cStr = cStr[:-2] + ")"
-
-    entries = mw.col.db.all(
-        "select cid, ease, time, type "
-        "from revlog where cid in %s" %(cStr))
     
+    cardNameById = {}
+    for k,v in cardOrdById.items():
+        for temp in templates:
+            if temp['ord'] == v:
+                cardNameById[k] = temp['name']
+
+
+
+    entries = mw.col.db.all("select id, cid, ease, ivl, time, type from revlog where cid in %s" %(cStr))
+
     hasReview = False
     if entries:
-        for (_, _, _, ty) in entries:
+        for (_, _, _, _, _, ty) in entries:
                 if ty == 1:
                     hasReview = True
                     break
+    
+    reviewPlotData = {}
+   
     if not entries or not hasReview:
-        s = "<hr/>No card has been reviewed yet for this note."
+        infoTable["Result"] = "No cards have been reviewed yet for this note"
+        tables.append(infoTable)
     else:
         cnt = 0
         passed = 0
         failed = 0
+        easy = 0
         goodAndEasy = 0
+        good = 0
         hard = 0
         timeTaken = 0
-        for (cid, ease, taken, ty) in reversed(entries):
+        intervalsByName = {}
+        for (stamp, cid, ease, ivl, taken, ty) in entries:
             #only look for reviews
             if ty != 1:
                 continue
             cnt += 1
+            intervalsByName[cardNameById[cid]] = ivl
+            if not cardNameById[cid] in reviewPlotData:
+                reviewPlotData[cardNameById[cid]] = []
+            reviewPlotData[cardNameById[cid]].append([cnt, ease, time.strftime("%Y-%m-%d", time.localtime(int(stamp)/1000))])
             if ease != 1:
                 passed += 1
                 if ease == 2:
                     hard += 1
                 else:
                     goodAndEasy += 1
+                    if ease == 3:
+                        good += 1
+                    else:
+                        easy += 1
             else:
                 failed += 1
             
-            timeTaken += taken  / 1000.0        
+            timeTaken += taken  / 1000.0 
+       
+         
         retention =  100 * passed / (passed + failed) if cnt > 0 else 0
         retention = round(retention, 1)
         avgTime = round(timeTaken / cnt, 1) if cnt > 0 else 0
         score = _calcPerformanceScore(retention, avgTime, goodAndEasy, hard) if cnt > 0 else (0, 0, 0, 0)
-        s = ""
-        s += "<div class='smallMarginTop'><span class='score'>Performance: %s</span><span class='minorScore'>Retention: %s</span><span class='minorScore'>Time: %s</span><span class='minorScore'>Ratings: %s</span></div><hr/>" %(str(score[0]), str(score[2]), str(score[1]), str(score[3]))
-        s += "<b>%s</b> card(s) found for this note.<br/><br/>" %(str(len(cards)))
-        if cnt > 0:
-            if avgRetAndTime is None:
-                avgRetAndTime = getAvgTrueRetentionAndTime()
-            if retention == 100.0:
-                s += "<div class='smallMarginBottom'>Your <b>retention</b> on cards of this note is <span class='darkGreen'>perfect</span>: <b>" + str(retention) + " %</b></div>"
-            elif retention >= 98.0:
-                s += "<div class='smallMarginBottom'>Your <b>retention</b> on cards of this note is <span class='darkGreen'>nearly perfect</span>: <b>" + str(retention) + " %</b></div>"
-            elif retention != avgRetAndTime[0]:
-                s += _compString("retention", _getCompExp("retention", retention, avgRetAndTime[0]), retention, calcAbsDiffInPercent(retention, avgRetAndTime[0]), "%")  
-            else: 
-                s += "<div class='smallMarginBottom'>Your <b>retention</b> on cards of this note is equal to your average retention: <b>" + str(retention) + " %</b></div>"
+        tables.append(infoTable)
+        infoTable = {}        
+        
+        infoTable["Cards Found"] = len(cards)
+        tables.append(infoTable)
+        
+        for k,v in cardNameById.items():
+            infoTable = {}
+            infoTable["<b>%s</b>:" % v] = ""
+            if v in intervalsByName:
+                infoTable["Interval"] = "%s %s" % (abs(intervalsByName[v]), "Days" if intervalsByName[v] > 0 else "Seconds")
+            if k in cardTypeById:
+                infoTable["Type"] = cardTypeById[k]
+            tables.append(infoTable)
 
-        
-            if avgTime != avgRetAndTime[1]:
-                s += _compString("time", _getCompExp("time", avgTime, avgRetAndTime[1]), avgTime, calcAbsDiffInPercent(avgTime, avgRetAndTime[1]), "s")  
-            else:
-                s += "<div class='smallMarginBottom'>Your <b>time</b> on cards of this note is equal to your average time: <b>" + str(avgTime) + " s</b></div>"
-        
-    s = "<div id='i-%s' style='position: absolute; bottom: 3px; left: 0px; padding: 7px;'>%s</div>" %(nid,s) 
+      
+        infoTable = {}
+        infoTable["Reviews (Cards from this note)"] = cnt
+        infoTable["Reviews - <span style='color: red'>Failed</span>"] = failed
+        infoTable["Reviews - <span style='color: black'>Hard</span>"] = hard
+        infoTable["Reviews - <span style='color: green'>Good</span>"] = good
+        infoTable["Reviews - <span style='color: blue'>Easy</span>"] = easy
+        tables.append(infoTable)
+
+      
+
+        if cnt > 0:
+            avgRetAndTime = getAvgTrueRetentionAndTime()
+            infoTable = {}
+            infoTable["True Retention (Cards from this note)"] = retention
+            infoTable["True Retention (Collection)"] = avgRetAndTime[0]
+            infoTable["True Retention (Difference)"] =  str(calcAbsDiffInPercent(retention, avgRetAndTime[0])) + "%"
+            tables.append(infoTable)    
+
+            infoTable = {}
+            infoTable["Average Time (Cards from this note)"] = avgTime
+            infoTable["Average Time (Collection)"] = avgRetAndTime[1]
+            infoTable["Average Time (Difference)"] = str(calcAbsDiffInPercent(avgTime, avgRetAndTime[1])) + "s"
+            tables.append(infoTable)
+           
+
+        infoTable = {}
+        infoTable["Retention Score"] = score[2]
+        infoTable["Time Score"] = score[1]
+        infoTable["Rating Score"] = score[3]
+        infoTable["-> Performance"] = score[0]
+        tables.append(infoTable)
     
+    return( _buildTable(tables, reviewPlotData), reviewPlotData)
+
+
+def _buildTable(tables, reviewPlotData):
+    s = "<div style='width: calc(100%% - 5px); overflow-y: auto; padding-right: 5px;'><table style='width: 100%%; margin-bottom: 5px;'>%s </table></div>" 
+    rows = ""
+    for table in tables:     
+        for key, value in table.items():
+            rows += "<tr><td>%s</td><td>%s</td></tr>" % (key, value)
+        rows += "<tr><td>&nbsp;</td><td>&nbsp;</td></tr>"
+    c = 0
+    s = s % rows
+    for k,v in reviewPlotData.items(): 
+        if len(v) > 1:
+            c+= 1
+            s += "<div><h3 style='margin-top: 10px;'>%s:</h3>" % k
+            s += "<div id='graph-" + str(c) + "' style='width: 96%; height: 300px; margin-top: 5px; margin-bottom: 40px;'></div></div>"
+
     return s
 
-def _getCompExp(field, value, avg):
-    bbb = 8
-    bb = 4
-    biggerIsBetter = True
-    if field == 'time':
-        bbb = 10
-        bb = 5
-        biggerIsBetter = False
 
-    if not biggerIsBetter:
-        v_h = value
-        value = avg
-        avg = v_h
+def _cardTypeStr(typeNumber):
+    if typeNumber == 0:
+        return "new"
+    if typeNumber == 1:
+        return "learning"
+    if typeNumber == 2:
+        return "due"
+    if typeNumber == 3:
+        return "filtered"
+    return "?"
 
-    if value > avg and value - avg >= bbb: 
-        return '<span class="darkGreen">significantly better</span>'
-    elif value > avg and value - avg >= bb:
-        return '<span class="green">better</span>'
-    elif value > avg:
-        return '<span class="lightGreen">slightly better</span>'
-    elif value < avg and avg - value >= bbb:
-        return '<span class="darkRed">significantly worse</span>'
-    elif value < avg and avg - value >= bb:
-        return '<span class="red">worse</span>'
-    elif value < avg:
-        return '<span class="lightRed">slightly worse</span>'
-    
-    
-    return "n.a."
-
-
-def _compString(fieldName, comp, value, diff, unit):
-    return "<div class='smallMarginBottom'>Your <b>%s</b> on cards of this note is %s than your average %s: <b>%s %s</b> (%s %s)</div>" %(fieldName, comp, fieldName, value, unit,  diff, unit) 
 
 def _calcPerformanceScore(retention, time, goodAndEasy, hard):
     if goodAndEasy == 0 and hard == 0:
