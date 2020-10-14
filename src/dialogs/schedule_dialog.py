047945b55d07f03372a1340f971df4b2eea88429
Tom Zeller <tom-kouran@gmx.de>
2020-10-14 08:56:44 +0200

    add icon

--- src/dialogs/schedule_dialog.py
+++ src/dialogs/schedule_dialog.py
@@ -40,6 +40,20 @@ class ScheduleDialog(QDialog):
 
         self.setLayout(QVBoxLayout())
 
+        c_lbl = QLabel(self)
+        c_icon   = "calendar_night.png" if state.night_mode else "calendar.png"
+        c_pixmap  = QPixmap(utility.misc.get_web_folder_path() + f"icons/{c_icon}").scaled(QSize(35, 35), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        c_lbl.setPixmap(c_pixmap)
+        hbox    = QHBoxLayout()
+        hbox.addStretch()
+        hbox.addWidget(c_lbl)
+        hbox.addStretch()
+        self.layout().addSpacing(10)
+        self.layout().addLayout(hbox)
+        self.layout().addSpacing(16)
+
+
+
         self.layout().addWidget(self.scheduler)
         accept = QPushButton("Save")
         accept.clicked.connect(self.accept)
