d0d4e25b10243e470e343509ecd0683ba67b1779
Tom Zeller <tom-kouran@gmx.de>
2020-10-18 22:50:31 +0200

    indexing data in state to prevent db access from other thread

--- src/state.py
+++ src/state.py
@@ -55,6 +55,9 @@ last_page_requested : Optional[int]                     = None
 
 shortcuts_failed    : List[str]                         = []
 
+index_data          : List[Any]                         = None
+index_data_size     : int                               = -1
+
 def check_index() -> bool:
     """ Returns True if index and ui are ready to use. """
     return (search_index is not None 
