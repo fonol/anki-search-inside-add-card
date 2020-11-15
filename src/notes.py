1f73f9cae41f9efbc857f3d0dc6c9420b96eab9d
Tom Zeller <tom-kouran@gmx.de>
2020-11-15 11:07:01 +0100

    fix error on last read

--- src/notes.py
+++ src/notes.py
@@ -1413,13 +1413,13 @@ def get_pdf_notes_last_added_first(limit : int = None) -> List[SiacNote]:
 
 def get_pdf_notes_last_read_first() -> List[SiacNote]:
     conn = _get_connection()
-    res = conn.execute("select notes.id,notes.title,notes.text,notes.source,notes.tags,notes.nid,notes.created,notes.modified,notes.reminder,notes.lastscheduled,notes.position,notes.extract_start,notes.extract_end,notes.delay from notes join read on notes.id == read.nid where lower(notes.source) like '%.pdf' group by notes.id order by max(read.created) desc").fetchall()
+    res = conn.execute("select notes.* from notes join read on notes.id == read.nid where lower(notes.source) like '%.pdf' group by notes.id order by max(read.created) desc").fetchall()
     conn.close()
     return _to_notes(res)
 
 def get_pdf_notes_ordered_by_size(order: str) -> List[SiacNote]:
     conn = _get_connection()
-    res = conn.execute(f"select notes.id,notes.title,notes.text,notes.source,notes.tags,notes.nid,notes.created,notes.modified,notes.reminder,notes.lastscheduled,notes.position,notes.extract_start,notes.extract_end,notes.delay from notes join read on notes.id == read.nid where lower(notes.source) like '%.pdf' group by notes.id order by max(read.pagestotal) {order}").fetchall()
+    res = conn.execute(f"select notes.* from notes join read on notes.id == read.nid where lower(notes.source) like '%.pdf' group by notes.id order by max(read.pagestotal) {order}").fetchall()
     conn.close()
     return _to_notes(res)
 
