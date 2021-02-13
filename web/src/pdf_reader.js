4c7f21b914a0754fe5c2c5b61c8c275afe89075e
Tom Zeller <tom-kouran@gmx.de>
2021-02-13 21:25:11 +0100

    fix contains not transpiling

--- web/src/pdf_reader.js
+++ web/src/pdf_reader.js
@@ -416,7 +416,7 @@ window.rerenderPDFPage = function (num, shouldScrollUp = true, fitToPage = false
                     if (fetchHighlights) {
                         updatePdfDisplayedMarks(true);
                     }
-                    if (["Sand", "Peach", "Night", "X1", "X2", "Mud", "Coral", "Moss", "Spooky"].contains(pdfColorMode)) {
+                    if (["Sand", "Peach", "Night", "X1", "X2", "Mud", "Coral", "Moss", "Spooky"].indexOf(pdfColorMode) !== -1) {
                         invertCanvas(ctx);
                     }
                 }
